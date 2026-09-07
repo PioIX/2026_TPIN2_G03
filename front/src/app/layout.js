47aff3f8efbe481eeb17b18626887296199b3f6b
Bzona10 <abonasegna@pioix.edu.ar>
2026-09-07 08:08:37 -0300

    división de carpetas

---
+++ front/src/app/layout.js
@@ -0,0 +1,24 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Notas",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ front/src/app/page.js
@@ -0,0 +1,13 @@
+"use client"
+import { useSearchParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function HomePage() {
+
+
+  return (
+    <>
+    </>
+  );
+}
