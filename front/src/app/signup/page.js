b58a9fcfea9651895f31220004ecfde165634a3f
fmartinPioIX <fmartin@pioix.edu.ar>
2026-09-10 08:39:00 -0300

    avances en sign up
    proximamente añadir imagenes y tener todo en una sola web condicional
    
    Co-Authored-By: fcatalano-glitch <276283379+fcatalano-glitch@users.noreply.github.com>

--- front/src/app/signup/page.js
+++ front/src/app/signup/page.js
@@ -4,31 +4,45 @@ import { useState } from 'react';
 
 export default function SignupPage() {
 
-    const [mensaje, setMensaje] = useState("")
+    const [mensajeCorreo, setMensajeCorreo] = useState("")
+    const [mensajeContrasena, setMensajeContrasena] = useState("")
+    const [mensajeUsuario, setMensajeUsuario] = useState("")
     const [correo, setCorreo] = useState("");
     const [contrasena, setContrasena] = useState("");
+    const [usuario, setUsuario] = useState("")
+    const [pfp, setPfp] = useState("no ha cargado imagenes")
 
     function enviarSign(event) {
-        //aca con post a la BDD
-        //comrpobar info
-        if (contrasena == "" && correo == "" || contrasena == "" /*&& llamar a la funcion para que corrobore que no esta repetido el mail*/){
-            setMensaje("no valido")
-            console.log("el mensaje esta vacio")
+        if (contrasena == "") {
+            setMensajeContrasena("no valido")
         } else {
-            setMensaje("valido")
-            /*post de correo*/
+            setMensajeContrasena("valido")
         }
-    }
-
-    function mailSign() {
+        if (correo == "" /*|| llamar a la funcion para que corrobore que no esta repetido el mail*/) {
+            setMensajeCorreo("no valido")
+        } else {
+            setMensajeCorreo("valido")
+        }
+        if (usuario == "") {
+            setMensajeUsuario("no valido")
+        } else {
+            setMensajeUsuario("valido")
+        }
+        if (pfp !== "" && usuario !== "" && contrasena !== "" && correo !== "" /*&& llamar a la funcion para que corrobore que no esta repetido el mail*/){
+            //aca con post a la BDD
+        }
     }
 
     return (
         <>
             <input placeholder="Establezca un correo electronico" value={correo} onChange={(event)=>{setCorreo(event.target.value)}}></input>
-            <p>{mensaje}</p>
-            <input placeholder="Establezca una contraseña" value={contrasena} onChange={(event)=>{setCorreo(event.target.value)}}></input>
-            <p>{mensaje}</p>
+            <p>{mensajeCorreo}</p>
+            <input placeholder="Establezca una contraseña" value={contrasena} onChange={(event)=>{setContrasena(event.target.value)}}></input>
+            <p>{mensajeContrasena}</p>
+            <input placeholder="Establezca un nombre de usuario" value={usuario} onChange={(event)=>{setUsuario(event.target.value)}}></input>
+            <p>{mensajeUsuario}</p>
+            <input type="file" placeholder="Establezca una foto de perfil" value={pfp} onChange={(event)=>{setPfp(event.target.value)}}></input>
+            <p>{pfp}</p>
 
             <button onClick={enviarSign}>REGISTRAR</button>
         </>
