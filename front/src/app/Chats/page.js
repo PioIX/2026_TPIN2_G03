05c73e9cbb339d8f791ce2511137d475861dcfe0
abonasegna <abonasegna@pioix.edu.ar>
2026-09-14 08:47:31 -0300

    arreglo

--- front/src/app/Chats/page.js
+++ front/src/app/Chats/page.js
@@ -9,15 +9,22 @@ import * as fetch from "@/hooks/fetch.js"
 export default function ChatPage() {
     
 const [error,setError]=useState(false)
-const [usuario,setUsuario]=useState(false)
+const [usuario,setUsuario]=useState({})
+const [loading,setLoading]=useState(false)
 
 const searchParams = useSearchParams();
 const userE = searchParams.get("usuario")
 
 useEffect(()=>{
     if(!searchParams.has("usuario")){setError(true)}
-    let us = fetch.getUsuarioporEmail("userE").then(setUsuario(us))
-    
+    else{
+        fetch.getUsuarioporEmail("userE")
+        .then((data)=>{
+        console.log(data)
+        setUsuario(data)
+        setLoading(false)
+      })
+    }
 },[])
 
   return (
