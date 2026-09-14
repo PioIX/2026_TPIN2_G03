e7fbaef36b090ce4667e2dca9722caf9006d5fd5
fcatalano-glitch <fcatalano@pioix.edu.ar>
2026-09-14 08:42:32 -0300

    merge para tener fetch

--- front/src/app/signup/page.js
+++ front/src/app/signup/page.js
@@ -10,7 +10,7 @@ export default function SignupPage() {
     const [correo, setCorreo] = useState("");
     const [contrasena, setContrasena] = useState("");
     const [usuario, setUsuario] = useState("")
-    const [pfp, setPfp] = useState("no ha cargado imagenes")
+    const [pfp, setPfp] = useState("")
 
     function enviarSign(event) {
         if (contrasena == "") {
@@ -28,7 +28,19 @@ export default function SignupPage() {
         } else {
             setMensajeUsuario("valido")
         }
-        if (pfp !== "" && usuario !== "" && contrasena !== "" && correo !== "" /*&& llamar a la funcion para que corrobore que no esta repetido el mail*/){
+        if (pfp == ""){
+            setPfp("default.png")
+        } else if (pfp == "1") {
+            setPfp("pfp1.png")
+        } else if (pfp == "2") {
+            setPfp("pfp2.png")
+        } else if (pfp == "3") {
+            setPfp("pfp3.png")
+        } else if (pfp == "4") {
+            setPfp("pfp4.png")
+        }
+
+        if (usuario !== "" && contrasena !== "" && correo !== "" /*&& llamar a la funcion para que corrobore que no esta repetido el mail*/){
             //aca con post a la BDD
         }
     }
@@ -41,8 +53,26 @@ export default function SignupPage() {
             <p>{mensajeContrasena}</p>
             <input placeholder="Establezca un nombre de usuario" value={usuario} onChange={(event)=>{setUsuario(event.target.value)}}></input>
             <p>{mensajeUsuario}</p>
-            <input type="file" placeholder="Establezca una foto de perfil" value={pfp} onChange={(event)=>{setPfp(event.target.value)}}></input>
-            <p>{pfp}</p>
+            <input placeholder="Establezca una foto de perfil segun su numero" value={pfp} onChange={(event)=>{setPfp(event.target.value)}}></input>
+
+            <div>
+                <div>
+                    <img src='pfp1.png'></img>
+                    <p>1</p>
+                </div>
+                <div>
+                    <img src='pfp2.png'></img>
+                    <p>2</p>
+                </div>
+                <div>
+                    <img src='pfp3.png'></img>
+                    <p>3</p>
+                </div>
+                <div>
+                    <img src='pfp4.png'></img>
+                    <p>4</p>
+                </div>
+            </div>
 
             <button onClick={enviarSign}>REGISTRAR</button>
         </>
